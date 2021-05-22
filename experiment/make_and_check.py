673f2a13e091eed9132d51aa27fb0b8af784b1e7
kdh9949 <kdh9949@snu.ac.kr>
2021-05-22 07:41:43 +0000

    fix experiment script a little bit

--- experiment/make_and_check.py
+++ experiment/make_and_check.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3.8
 
+import sys
 from os import system, listdir
 from time import time
 S = system
 
+limit = 60
+if len(sys.argv) == 2:
+  limit = int(sys.argv[1])
+
 S('make -C ../build')
 
 query_list = sorted(listdir('../query'))
+tot_cor = 0
+tot_incor = 0
+tot_time = 0
 for query_name in query_list:
   base_name = query_name.split('.')[0]
   qpath = '../query/' + query_name
@@ -18,8 +26,31 @@ for query_name in query_list:
   print('---------------------------------')
   print('')
   print(f'Test for ({dpath}, {qpath})')
+  
   stime = time()
-  S(f'timeout 60 ../build/main/program {dpath} {qpath} {cpath} > {opath}')
+  S(f'timeout {limit} ../build/main/program {dpath} {qpath} {cpath} > {opath}')
   etime = time()
   print(f'Elapsed time : {etime - stime:.3f} seconds')
-  S(f'./score {dpath} {qpath} {opath}')
+  tot_time += etime - stime
+  
+  S(f'./score {dpath} {qpath} {opath} > ../tmp/score.out')
+  g = open('../tmp/score.out', 'r')
+  line = g.readline()
+  print(line, end='')
+  cor = int(line.split()[-1])
+  line = g.readline()
+  print(line, end='')
+  incor = int(line.split()[-1])
+  g.close()
+  tot_cor += cor
+  tot_incor += incor
+
+tot_cnt = len(query_list)
+print('')
+print('*********************************************')
+print('            EXPERIMENT RESULT')
+print('*********************************************')
+print('')
+print(f'Average Elapsed time  : {tot_time / tot_cnt:.3f} seconds')
+print(f'Average Correct Ans   : {tot_cor / tot_cnt:.3f}')
+print(f'Average Incorrect Ans : {tot_incor / tot_cnt:.3f}')
